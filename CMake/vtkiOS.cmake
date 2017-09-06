0b04901e5b6e5db2be6b9c3ae18274f901c3858b
Alexis Girault <alexis.girault@kitware.com>
2017-09-06 13:09:12 -0400

    IRIS: Expose vtkDICOM and vtkRenderingImage modules

--- CMake/vtkiOS.cmake
+++ CMake/vtkiOS.cmake
@@ -94,6 +94,7 @@ mark_as_advanced(
 option(Module_vtkRenderingOpenGL2 "Include Polygonal Rendering Support" ON)
 option(Module_vtkInteractionWidgets "Include InteractionWidgets module" OFF)
 option(Module_vtkIOXML "Include IO/XML Module" OFF)
+option(Module_vtkDICOM "Turn on or off this module" OFF)
 option(Module_vtkFiltersModeling "Turn on or off this module" OFF)
 option(Module_vtkFiltersSources "Turn on or off this module" OFF)
 option(Module_vtkIOGeometry "Turn on or off this module" OFF)
@@ -102,6 +103,7 @@ option(Module_vtkIOImage "Turn on or off this module" OFF)
 option(Module_vtkIOPLY "Turn on or off this module" OFF)
 option(Module_vtkIOInfovis "Turn on or off this module" OFF)
 option(Module_vtkRenderingFreeType "Turn on or off this module" OFF)
+option(Module_vtkRenderingImage "Turn on or off this module" OFF)
 option(Module_vtkRenderingVolumeOpenGL2 "Include Volume Rendering Support" ON)
 option(Module_vtkRenderingLOD "Include LOD Rendering Support" OFF)
 
@@ -125,6 +127,7 @@ set(ios_cmake_flags
   -DModule_vtkRenderingOpenGL2:BOOL=${Module_vtkRenderingOpenGL2}
   -DModule_vtkInteractionWidgets:BOOL=${Module_vtkInteractionWidgets}
   -DModule_vtkIOXML:BOOL=${Module_vtkIOXML}
+  -DModule_vtkDICOM:BOOL=${Module_vtkDICOM}
   -DModule_vtkFiltersModeling:BOOL=${Module_vtkFiltersModeling}
   -DModule_vtkFiltersSources:BOOL=${Module_vtkFiltersSources}
   -DModule_vtkIOGeometry:BOOL=${Module_vtkIOGeometry}
@@ -133,9 +136,17 @@ set(ios_cmake_flags
   -DModule_vtkIOPLY:BOOL=${Module_vtkIOPLY}
   -DModule_vtkIOInfovis:BOOL=${Module_vtkIOInfovis}
   -DModule_vtkRenderingFreeType:BOOL=${Module_vtkRenderingFreeType}
+  -DModule_vtkRenderingImage:BOOL=${Module_vtkRenderingImage}
   -DModule_vtkRenderingVolumeOpenGL2:BOOL=${Module_vtkRenderingVolumeOpenGL2}
   -DModule_vtkRenderingLOD:BOOL=${Module_vtkRenderingLOD}
 )
+
+if (Module_vtkDICOM AND IOS_EMBED_BITCODE)
+  # libvtkzlib does not contain bitcode
+  list (APPEND ios_cmake_flags
+    -DBUILD_DICOM_PROGRAMS:BOOL=OFF
+    )
+endif()
 
 if (Module_vtkRenderingOpenGL2 OR Module_vtkRenderingVolumeOpenGL2)
   list (APPEND ios_cmake_flags
--- Remote/vtkDICOM.remote.cmake
+++ Remote/vtkDICOM.remote.cmake
@@ -5,6 +5,6 @@
 vtk_fetch_module(vtkDICOM
   "Dicom classes and utilities"
   GIT_REPOSITORY https://github.com/dgobbi/vtk-dicom
-  # vtk-dicom with override, delete for C++11 (Feb 10, 2017)
-  GIT_TAG 9a4c5c3c23b952f17f2ed540cf60f932f9e1262c
+  # vtk-dicom with quote headers for ios (Sept 5, 2017)
+  GIT_TAG 0090d79ee1cc98b8d33fcfde5dda2c983e798324
   )
